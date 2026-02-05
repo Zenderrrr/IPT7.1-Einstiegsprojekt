beabc88e00b6f5ac09de33d76f1ebc702208fffe
b̵̛̩̖̳̙̩̲̐͗̏͒̈̕0̵̟͖̰͈̝̄̈́̋̄͊̚͝b̶̧̠͇̹̪̲̼̪̄́̓̔ë̸̝͕̗͓́͂͌̐̚ͅr̵̨͍̰͈̬̭͑̑̇ <125749439+Zenderrrr@users.noreply.github.com>
2026-02-05 23:04:07 +0100

    fixed .js and App.vue files

--- login-system/src/App.vue
+++ login-system/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Header from "./components/Header.vue";
 import { RouterView } from "vue-router";
+import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
-import LoginView from "./views/LoginView.vue";
 </script>
 
 <template>
-  <!-- temporarily -->
-  <LoginView />
+  <Header />
+  <RouterView />
+  <Footer />
 </template>
--- login-system/src/router/index.js
+++ login-system/src/router/index.js
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "../views/LoginView.vue";
+import RegisterView from "../views/RegistrationView.vue";
+import ProductsView from "../views/ProductsView.vue";
 
 const routes = [
-  {
-    path: "/login",
-    component: LoginView,
-  },
+  { path: "/", redirect: "/login" },
+  { path: "/login", component: LoginView },
+  { path: "/register", component: RegisterView },
+  { path: "/products", component: ProductsView },
 ];
 
-const router = createRouter({
+export default createRouter({
   history: createWebHistory(),
   routes,
 });
-
-export default router;

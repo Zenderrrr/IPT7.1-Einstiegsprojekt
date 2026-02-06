bdb430d156e970305a17bd682805a1732957165f
Egor <125749439+Zenderrrr@users.noreply.github.com>
2026-02-06 12:44:01 +0100

    minor changes in routers

--- login-system/src/router/index.js
+++ login-system/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "../views/LoginView.vue";
-import RegisterView from "../views/RegistrationView.vue";
+import RegistrationView from "../views/RegistrationView.vue";
 import ProductsView from "../views/ProductsView.vue";
 
 const routes = [
   { path: "/", redirect: "/login" },
   { path: "/login", component: LoginView },
-  { path: "/register", component: RegisterView },
+  { path: "/registration", component: RegistrationView },
   { path: "/products", component: ProductsView },
 ];
 
